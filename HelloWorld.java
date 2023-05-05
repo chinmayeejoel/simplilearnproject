d013bda19e4354ef50a1ab71edf6e67426682ca8
chinmayeejoel <127047659+chinmayeejoel@users.noreply.github.com>
2023-05-05 18:54:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	   public static void main(String[] args) {
 
-		  System.out.println("Hello, world! for the 2nd time edited");
+		  System.out.println("Hello, world! the 2nd time edited");
 
 			 }
 }
