c5ff3a81aa3b88f5ab62f2099ad3207c81eb5ce9
chinmayeejoel <127047659+chinmayeejoel@users.noreply.github.com>
2023-05-05 17:54:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	   public static void main(String[] args) {
 
-		         System.out.println("Hello, world! testing after 2nd time editing");
+		         System.out.println("Hello, world! for the 1st project");
 
 			 }
 }
